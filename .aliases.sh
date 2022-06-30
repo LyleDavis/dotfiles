77bf12c2e14cf395550b68a35166daf786733653
Lyle Davis <me@lyledavis.dev>
2022-06-30 20:19:19 +0000

    remove gitcutter; add aliases

---
+++ .aliases.sh
@@ -0,0 +1,11 @@
+# aliases
+alias stop_all_containers='docker stop $(docker ps -q -a)'
+#alias cat='bat'
+#alias catp='bat --style=plain'
+alias lzd='lazydocker'
+alias ll='ls -al'
+alias gst='git status'
+alias gcm='git commit'
+alias dps='docker ps'
+alias w='watch'
+alias localstack='aws --endpoint-url=http://localhost:4566'
--- .zshrc
+++ .zshrc
@@ -1,4 +1,5 @@
 source ~/.credentials.sh
+source ~/.aliases.sh
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
