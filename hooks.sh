17ab98a7400564d60f67ac44222ede582e15dab5
Lyle Davis <me@lyledavis.dev>
2023-05-09 19:43:39 +0000

    add homebrew to hooks

--- hooks.sh
+++ hooks.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-[ -d "/opt/homebrew" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
-type -P rbenv &> /dev/null && eval "$(rbenv init - bash)"
+if [ -d /opt/homebrew ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  type -P rbenv &> /dev/null && eval "$(rbenv init - bash)"
+else
+  if [ -d ~/.rbenv ]; then
+    eval "$(~/.rbenv/bin/rbenv init - bash)"
+  fi
+fi
+
+#[ -d "/opt/homebrew" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
+# if homebrew exists, assume rbenv is already in path
+#[ -d "/opt/homebrew" ] && type -P rbenv &> /dev/null && eval "$(rbenv init - bash)"
+# if homebrew does not exist and rbenv has been cloned then use that to initialise
+#[ ! -d "/opt/homebrew" ] && [ -d "~/.rbenv" ] && eval "$(~/.rbenv/bin/rbenv init - bash)"
--- shell_options.sh
+++ shell_options.sh
@@ -2,5 +2,10 @@
 
 shopt -q autocd cdspell checkjobs checkwinsize cmdhist dirspell
 bind 'set completion-ignore-case on'
+
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
 # get gpg password for commit signing from command line
 export GPG_TTY=$(tty)
