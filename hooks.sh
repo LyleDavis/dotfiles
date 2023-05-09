27f3c369380d1afb89ae82e5d3ed1ce87a47bbe6
Lyle Davis <me@lyledavis.dev>
2023-05-09 19:46:24 +0000

    add hooks for nvm etc

--- hooks.sh
+++ hooks.sh
@@ -9,6 +9,14 @@ else
   fi
 fi
 
+[ -d "/usr/local/go" ] && export PATH=$PATH:/usr/local/go/bin
+
+if [ -d "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
 #[ -d "/opt/homebrew" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
 # if homebrew exists, assume rbenv is already in path
 #[ -d "/opt/homebrew" ] && type -P rbenv &> /dev/null && eval "$(rbenv init - bash)"
