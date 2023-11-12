6c0cc9b48c98bbb7aecfea5f081eabc6104b43e8
Lyle Davis <lyle.davis@sage.com>
2023-11-12 16:19:55 +0000

    change commit signing mechanism to 1p managed ssh

--- install.sh
+++ install.sh
@@ -24,3 +24,6 @@ replace ".p10k.zsh"
 replace ".zshrc"
 replace "golang_vars.sh"
 replace ".zshenv"
+
+# symlink to .1password for more standardised access
+mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
